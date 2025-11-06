be981dc708f1c1f218a9bf6cede011691ccc9cd2
Puspa Adhikari <pusparajadhikari0@yahoo.com>
2025-11-06 18:47:05 -0500

    added arrow function solution

--- palindrome.js
+++ palindrome.js
@@ -11,3 +11,20 @@ function checkPalindrome (str) {
 
 console.log(checkPalindrome("Hello"));
 console.log(checkPalindrome("MADAM"));
+
+// Arrow function
+
+const checkPalindrome2 = (str2) => {
+  const cleaned2 = str2.toLowerCase().replace(/[^-z0-9]/g, "");
+
+  for (let i=0; i<cleaned2.length/2; i++){
+    if (cleaned2[i] !== cleaned2[cleaned2.length - 1 - i]){
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(checkPalindrome2("Hello"));
+console.log(checkPalindrome2("MADAM"));
